c82cb54fd71241dbc47d538470e115f0ba9b11cb
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2023-05-03 13:09:39 +0200

    présentation du jeu

--- Main.java
+++ Main.java
@@ -17,25 +17,49 @@ public class Main {
             }
         }
         String nameOrdi = "Yumi";
-        
+        int nbPiece = 18;
+        int nbDomino = 3;
+        int nbTriomino = 6;
+        int nbTetromino = 9;
 
         // affichage à l'écran
-        Ecran.afficherln("Saisisez votre pseudo: ");
+        Ecran.afficher("Saisisez votre pseudo: ");
         String nameJoueur = Clavier.saisirString();
+        Ecran.sautDeLigne();
+        // Runtime.getRuntime().exec("clear");
         Ecran.afficherln("Bonjour " + ANSI_BLUE, nameJoueur, ANSI_RESET +", bienvenu sur notre jeu !");
-        Ecran.afficherln("Vous allez jouer contre "+ ANSI_RED, nameOrdi, ANSI_RESET+" (l'ordinateur).");
+        Ecran.afficherln("Vous allez jouer contre "+ ANSI_RED, nameOrdi, ANSI_RESET+", notre IA.");
+        Ecran.sautDeLigne();
 
+        Ecran.afficherln("Voici la grille de jeu:");
         displayGrid(Grid);
-        Ecran.afficherln("Voici les pièces dont vous disposez:");
+        Ecran.sautDeLigne();
+
+        Ecran.afficherln(ANSI_BLUE, nameJoueur, ANSI_RESET +", vous disposez de ",nbPiece ," pièces, dont ", nbDomino, " dominos(2cases), ", nbTriomino," triominos(3cases) et de ", nbTetromino, " tétrominos(4cases)."); 
+        Ecran.afficherln(ANSI_RED, nameOrdi, ANSI_RESET+" possède les mêmes pièces que vous.");
+        Ecran.afficherln("Voici les différentes pièces dont vous disposez:");
+        /////////////////////////////////////////////////////////////////////////////////////
         // faire l'affichage de toutes les pieces possibles (appel à toString)
-        Ecran.afficherln("Vous désirez poser quelle pièce ? (entrez le numéro de la pièce)");
+
+        Ecran.sautDeLigne();
+        Ecran.afficher(ANSI_BLUE, nameJoueur, ANSI_RESET +", c'est à vous de commencer ! ");
+        Ecran.afficher("Vous désirez poser quelle pièce ? (entrez le numéro de la pièce)");
         int pieceCoisi = Clavier.saisirInt();
-        Ecran.afficherln("Vous désirez poser la pièce choisie dans quelle disposition ? (entrez le numéro de la pièce)");
+
+        Ecran.afficherln("Voici les dispositions possibles pour cette pièce:");
+        /////////////////////////////////////////////////////////////////////////////////////
         // faire l'affichage de la piece selectionner dans toutes les dispositions (appel à toString)
+
+        Ecran.sautDeLigne();
+        Ecran.afficher(ANSI_BLUE, nameJoueur, ANSI_RESET + ", vous désirez poser la pièce choisie dans quelle disposition ? (entrez le numéro de la pièce)");
+
         int pieceDisposition = Clavier.saisirInt();
-        Ecran.afficherln("Vous désirez poser la pièce choisie à quel endroit ? (entrez la lettre de la colonne puis le numéro de la ligne)");
+        Ecran.afficher(ANSI_BLUE, nameJoueur, ANSI_RESET +", vous désirez poser la pièce choisie à quel endroit ? (entrez la lettre de la colonne puis le numéro de la ligne)");
         String positionPiecePlace = Clavier.saisirString();
+
+        /////////////////////////////////////////////////////////////////////////////////////
         // transformer la chaine en position
+
         displayGrid(Grid); // l'afficher avec la pièce posée
     }
 
